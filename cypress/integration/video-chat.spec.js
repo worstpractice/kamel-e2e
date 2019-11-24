0e48e3163c3c7642affdc4ad51a426c59305d53b
SaltErik <erik.nygren@appliedtechnology.se>
2019-11-24 17:46:20 +0100

    added e2e test for video chat

---
+++ cypress/integration/video-chat.spec.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// NOTE: This test only works if you already have somebody logged onto the server
+//       so the test has someone to talk to.
+
+context('a user wants to video chat', () => {
+
+  it('steps through the whole procedure of starting up a video chat', () => {
+    cy.visit('/');
+
+    cy.get('button')
+      .contains(/login/i)
+      .click();
+
+    cy.get('.chat-board > h3')
+      .should('have.text', 'Select contact')
+      .and('be.visible');
+
+    cy.get('li')
+      .click();
+
+    cy.get('.chat-board > h3')
+      .should('not.have.text', 'Select contact')
+      .and('be.visible');
+
+    cy.get('span')
+      .click();
+
+    // At the moment, this blows up
+  });
+});
