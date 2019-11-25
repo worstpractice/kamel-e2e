f837da01691f5e61cd7edf85639a531892ddb080
SaltErik <erik.nygren@appliedtechnology.se>
2019-11-25 12:02:49 +0100

    e2e test for signup process

---
+++ cypress/integration/user flows/signup.spec.js
@@ -0,0 +1,34 @@
+/// <reference types="cypress" />
+'use strict';
+const Chance = require('chance');
+
+const chance = new Chance();
+
+const username = chance.username();
+const password = chance.password();
+const email = chance.email();
+const language = 'fr';
+
+context('a user wants to sign up', () => {
+
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('signs up to the site', () => {
+    cy.get('input[name=username]')
+      .type(username);
+
+    cy.get('input[name=email]')
+      .type(email);
+
+    cy.get('input[name=password]')
+      .type(password);
+
+    cy.get('input[name=language]')
+      .type(language);
+
+    cy.get('button')
+      .click({ multiple: true });
+  });
+});
