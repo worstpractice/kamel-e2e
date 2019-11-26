fa46bde0ae1251efc986e6cf7045797473640322
SaltErik <erik.nygren@appliedtechnology.se>
2019-11-26 15:45:13 +0100

    signup flow complete BOO-YAH

---
+++ cypress/lib/random.js
@@ -0,0 +1,38 @@
+'use strict';
+const Chance = require('chance');
+
+const chance = new Chance();
+
+chance.mixin({
+  domain() {
+    const domains = ['se', 'com', 'org', 'net', 'de', 'es'];
+    return `${chance.pickone(domains)}`.replace(/[^a-z0-9]/gi, '');
+  },
+});
+
+chance.mixin({
+  password() {
+    return `${chance.animal().toLowerCase()}${chance.integer().toString().slice(0, 3)}`.replace(/[^a-z0-9]/gi, '');
+  },
+});
+
+chance.mixin({
+  email() {
+    return {
+      email: `${chance.word().toLowerCase()}@${chance.word()}.${chance.url()}`.replace(/[^a-z0-9]/gi, ''),
+    };
+  },
+});
+
+chance.mixin({
+  user() {
+    const [firstName, lastName] = chance.name().split(' ');
+    return {
+      name: `${firstName} ${lastName}`,
+      password: chance.password(),
+      email: `${firstName}.${lastName}@${chance.word()}.${chance.domain()}`.toLowerCase(),
+    };
+  },
+});
+
+module.exports = { random: chance };
